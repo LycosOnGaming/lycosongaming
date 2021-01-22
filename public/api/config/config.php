7628b4c0221d15691140b37152ed418a7e3b829a
LycosOnGaming <77142861+LycosOnGaming@users.noreply.github.com>
2021-01-22 11:51:57 +0100

    Delete config.php

--- public/api/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'mysql27.1blu.de';
-$user = 's292110_2897252';
-$database = 'db292110x2897252';
-$password = '#LycosOnGaming1';
-
-$dbPlannedGames = 'plannedGames';
-$dbStreamPartner = 'streamPartner';
-?>
