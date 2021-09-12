f6ceb579a4e94c4ea27364ee29bd68f93962befb
LycosOnGaming <77142861+LycosOnGaming@users.noreply.github.com>
2021-09-12 08:09:34 +0200

    Refactoring

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { NavLink as Link } from 'react-router-dom';
-import $ from "jquery";
 import data from './navigation.json';
 import Logo from '../../assets/images/moon_menu.png';
 
@@ -9,21 +8,24 @@ class FixedNavigation extends Component {
     constructor(props) {
         super(props);
         
-        this.toggleMenu= this.toggleMenu.bind(this);
         this.state = {
             isActive: false
         }
+
+        this.handleShowMenu = this.handleShowMenu.bind(this);
     }
-
-    toggleMenu(){
-        $('#navbar').toggleClass('open');
-    }    
-
+    
+    handleShowMenu() {
+        this.setState({
+            isActive: !this.state.isActive,
+        })
+    }
+        
     render() {
         return (
             <div className="Navigation">
-                <div id="navbar">
-                    <div id="navigation" onClick={this.toggleMenu}>
+                <div id="navbar" className={this.state.isActive ? "open" : ""}>
+                    <div id="navigation" onClick={this.handleShowMenu}>
                         <i className="fa fa-bars main-menu"></i>
                         <img src={Logo} alt="banner" />
                     </div>
@@ -32,7 +34,7 @@ class FixedNavigation extends Component {
                         return (
                             <li key={navitems.title + '_'} className="nav-item">
                                 <Link
-                                    onClick={this.toggleMenu}
+                                    onClick={this.handleShowMenu}
                                     exact={true}
                                     className="nav-link"
                                     to={navitems.src}
