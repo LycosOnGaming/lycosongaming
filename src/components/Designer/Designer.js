5cd11fd19686f9695d7b6c85d62bf0f34204a9e8
BenjaminRoth <benjamin.roth@prosales.gmbh>
2022-11-14 15:45:00 +0100

    improvement

--- src/components/Designer/Designer.js
+++ src/components/Designer/Designer.js
@@ -4,31 +4,72 @@ import Shorlock from '../../assets/images/Shorlock-normal.png';
 import './Designer.scss';
 
 class Designer extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isHover: false,
+		};
+	}
+
+	componentDidMount() {}
+
+	onMouseEnterHandler = (selected) => {
+		this.setState({
+			isHover: selected,
+		});
+	};
+
+	onMouseLeaveHandler = (selected) => {
+		this.setState({
+			isHover: !selected,
+		});
+	};
+
 	render() {
 		return (
 			<div className="Designer">
 				<div className="row">
 					<div className="col-12 text-center">
-						<h1>Shorlock</h1>
+						<h1>Designer</h1>
 					</div>
 					<div className="col-12 text-center">
 						<img
-							className="w-50"
+							className="w-25"
 							src={Shorlock}
+							onMouseEnter={() => this.onMouseEnterHandler()}
+							onMouseLeave={() => this.onMouseLeaveHandler()}
+							onClick={(e) => {
+								e.preventDefault();
+								window.open(
+									'//www.shorlocktv.de/portfolio/',
+									'_blank'
+								);
+							}}
 							alt=""
-							title={Shorlock}
+							title=""
 						/>
 					</div>
 					<div className="col-12 text-center mt-2">
-						Mein Name ist Nils Renschler und ich bin der Inhaber von
-						Shorlock Design. Designs und Animationen gehören schon
-						länger zu unserem Alltag, wir finden sie beim fernsehen,
-						auf Social Medias, in Schaufenstern und und und. Meine
+						Hier kommt Nils Renschler der Inhaber von Shorlock
+						Design. Designs und Animationen gehören schon länger zu
+						unserem Alltag, wir finden sie beim fernsehen, auf
+						Social Medias, in Schaufenstern und und und. Seine
 						Aufgabe ist es das perfekte Design oder die Perfekte
-						Animation für meine Kunden zu fertigen. Damit stehe ich
-						mit meiner 100% Zufriedenheitsgarantie ein. Bezahlung
-						verlange ich nur wenn der Kunde auch voll und ganz
-						überzeugt ist.
+						Animation für seine Kunden zu fertigen.
+					</div>
+					<div className="col-12 text-center mt-2">
+						100% Zufriedenheit wird Garantiert.
+					</div>
+					<div className="col-12 text-center mt-2">
+						Hier geht es zu
+						<Link
+							target="_blank"
+							to="//www.shorlocktv.de/portfolio/"
+						>
+							{' '}
+							Shorlock
+						</Link>
 					</div>
 				</div>
 			</div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import LycosLogo from '../../assets/images/LycosLogo.png';
 import lycosBanner from '../../assets/images/Untitled 4.jpg';
 import './Header.scss';
 
 class Header extends Component {
-    render() {
-        return (
-            <header className="container-fluid px-0 Header">
-                <div className="row">
-                    <div className="col-12">
-                        <img
-                            src={lycosBanner}
-                            alt="banner"
-                        />
-                    </div>
-                </div>
+	render() {
+		return (
+			<header className="container-fluid px-0 Header">
+				<div className="row">
+					{/* <div className="col-3 col-md-3 col-xl-2">
+						<Link to="/">
+							<img
+								className="d-none d-lg-inline logo w-75 position-absolute pl-2 pt-2"
+								src={LycosLogo}
+								alt="logo"
+							/>
+						</Link>
+					</div> */}
+					<div className="col-12">
+						<img
+							className="banner"
+							src={lycosBanner}
+							alt="banner"
+						/>
+					</div>
+				</div>
 			</header>
-        );
-    }
+		);
+	}
 }
 
 export default Header;
